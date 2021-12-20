47c4bcde61448d140904e2c1298e8d515a4dd7f7
faisal-kn <khanmohdfaisal75@gmail.com>
2021-12-20 22:57:55 +0530

    contacts page made

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,30 @@
+import styles from "./Contact.module.css";
+
 const Contact = () => {
-	return <div>Contact page</div>;
+  return (
+    <>
+      <h2 className={`container ${styles.contact}`}>
+        <span className={styles.subheading}>Contact</span> GDG Sricity
+      </h2>
+      <div className={`container ${styles.container}`}>
+        <p className={styles.maintext}>
+          Our events are open to newbies, developers, managers, and
+          organizations who are interested in Google's technologies or use them
+          as part of their projects.
+          <br />
+          <br /> Google Developers Group Jalandhar is an initiative to
+          concentrate the efforts of many developers in and around Punjab to
+          learn, share and get productive using the various Google products.
+          <br />
+          <br /> Questions? Please contact connectwithaurapp@gmail.com
+        </p>
+        <img
+          src="https://myaurapp.web.app/img/contact.23fa5966.svg"
+          alt="unimportant"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Contact;
