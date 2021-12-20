23073801c2339edef4c1b7bbb218c0b1b0ae874d
faisal-kn <khanmohdfaisal75@gmail.com>
2021-12-21 00:00:26 +0530

    made chages to contacts page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -12,11 +12,11 @@ const Contact = () => {
           organizations who are interested in Google's technologies or use them
           as part of their projects.
           <br />
-          <br /> Google Developers Group Jalandhar is an initiative to
-          concentrate the efforts of many developers in and around Punjab to
-          learn, share and get productive using the various Google products.
+          <br /> Google Developers Group Sricity is an initiative to concentrate
+          the efforts of many developers in and around to learn, share and get
+          productive using the various Google products.
           <br />
-          <br /> Questions? Please contact connectwithaurapp@gmail.com
+          <br /> Questions? Please contact us at{" "}
         </p>
         <img
           src="https://myaurapp.web.app/img/contact.23fa5966.svg"
